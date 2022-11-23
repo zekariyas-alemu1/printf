d7c41c3ad4c76bc8dbacdaf05b58671493b08b9b
mariams58 <mariams58@yahoo.com>
2022-11-23 14:01:05 -0800

    Fix: hanled format specifier

--- _printf.c
+++ _printf.c
@@ -9,27 +9,37 @@ int _printf(const char *format, ...)
 {
 	va_list list;
 	int i = 0, count;
-	char arg, *na;
+	char *na, arg;
 
 	while (format != NULL && format[i])
 	{
 		va_start(list, format);
-		if ((format[i - 1] != '%' && format[i] != '%') || format[i - 2] == '%')
-			_putchar(format[i]);
-		if (format[i] == '%' && format[i + 1] == 'c')
+		while(format[i] != '\0')
 		{
-			arg = (char)va_arg(list, int);
-			_putchar(arg);
-		}
-		else if (format[i] == '%' && format[i + 1] == 's')
-		{
-			na = va_arg(list, char*);
-			for (count = 0; na[count] != '\0'; count++)
-				_putchar(na[count]);
-		}
-		else if (format[i] == '%' && format[i + 1] == '%')
+			if (format [i - 1] == '%')
+				switch (format[i])
+				{
+					case 'c':
+						arg = va_arg(list, int);
+						_putchar(arg);
+						break;
+					case 's':
+						na = va_arg(list, char*);
+						if (na == NULL)
+							na = "(null)";
+						for(count = 0; na[count] != '\0'; count++)
+							_putchar(na[count]);
+						break;
+					case '%':
+						_putchar(37);
+						break;
+					default:
+						_putchar(format[i]);
+				}
+			else if (format[i] != '%')
 				_putchar(format[i]);
-		i++;
+			i++;
+		}
 	}
 	va_end(list);
 	return (i + count);
